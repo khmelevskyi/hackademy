4366eb207b29416464577da143c034bbb9f8c16b
Yevhen Khmelevskyi <khmellevskyi.ye23@gmail.com>
2021-09-12 16:05:39 +0300

    ex01 done

---
+++ courses/cunix/ex01/src/my_strlen.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  my_strlen.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/12/21 12:50:06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+unsigned int my_strlen(char *str)
+{
+	unsigned int strlen = 0;
+
+	while (*str++ != '\0') strlen++;
+
+	return strlen;
+}
